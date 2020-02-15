32138dc95c812adca41dd47ba8b6e760cd213bfe
BuildTools <unconfigured@null.spigotmc.org>
2020-02-15 15:16:49 +0100

    e

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`Welcome to ESC! | :help`);
+  client.user.setActivity(`Being a smol bean | -help`);
 });
 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Welcome to ESC! | :help`);
+  client.user.setActivity(`Being a smol bean | -help`);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Welcome to ESC! | :help`);
+  client.user.setActivity(`Being a smol bean | -help`);
 });
 
 client.on("message", async message => {
