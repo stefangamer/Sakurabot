8459d2db6f3ea22b7b499702642211feb4b6b323
BuildTools <unconfigured@null.spigotmc.org>
2020-02-16 15:38:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on("guildDelete", guild => {
   client.user.setActivity(`Being a smol bean | -help`);
 });
 
-client.on("message", async message => {
+client.on("message", async message => ((((((({
   // This event will run on every single message received, from any channel or DM.
   
   // It's good practice to ignore other bots. This also makes your bot ignore itself
