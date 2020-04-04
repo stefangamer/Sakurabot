754ec8bee0eddcb0dfb2a4213a40d88f08c1258c
BuildTools <unconfigured@null.spigotmc.org>
2020-04-04 13:45:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ client.on("message", async message => {
 }
 
     if (command === "say") {
-        if (message.member.roles.some(r => ["Dev", "Boterator", "Server Staff", "Emperor", "High Power", "DJ", "Administrator", "Technician PRO"].includes(r.name))) {
+ if (message.member.hasPermission('MANAGE_MESSAGES')){
             // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
             // To get the "message" itself we join the `args` back into a string with spaces: 
             const sayMessage = args.join(" ");
@@ -180,8 +180,8 @@ client.on("message", async message => {
             message.delete().catch(O_o => {});
             // And we get the bot to say the thing: 
             message.channel.send(sayMessage);
-        }
-    }
+ }
+	} else return message.reply("I won't let you control my body!")
 
     if (command === "kick") {
         // This command must be limited to the roles we wanna choose. In this example we just hardcode the role names.
@@ -244,72 +244,7 @@ if(command === "ban") {
       .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}.`));
     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}.`);
   }
-  
-      if(command === "forcemute") {
- if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No can do.");
-  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!tomute) return message.reply("Couldn't find user.");
-  if(tomute.hasPermission("ADMINISTRATOR")) return message.reply("Can't mute them!");
-  let reason = args.slice(2).join(" ");
-  if(!reason) return message.reply("Usage is !mute (user) (time in minutes) (reason)");
-
-  let muterole = message.guild.roles.find(`name`, "muted");
-  //start of create role
-  if(!muterole){
-    try{
-      muterole = await message.guild.createRole({
-        name: "muted",
-        color: "#000000",
-        permissions:[]
-      })
-      message.guild.channels.forEach(async (channel, id) => {
-        await channel.overwritePermissions(muterole, {
-          SEND_MESSAGES: false,
-          ADD_REACTIONS: false
-        });
-      });
-    }catch(e){
-      console.log(e.stack);
-    }
-  }
-  //end of create role
-  let mutetime = args[1]
-    let muteminutes = mutetime * 60000
-   timeInt = parseInt(muteminutes)
-
-  message.delete().catch(O_o=>{});
-
-  try{
-    await tomute.send(`Hi! You've been muted for ${mutetime} minute(s). Sorry!`)
-  }catch(e){
-    message.channel.send(`A user has been muted... but their DMs are locked. They will be muted for ${mutetime} minutes`)
-  }
-
-  let muteembed = new Discord.RichEmbed()
-  .setDescription(`Mute executed by ${message.author}`)
-  .setColor("#0000000")
-  .addField("Muted User", tomute)
-  .addField("Muted in", message.channel)
-  .addField("Time", message.createdAt)
-  .addField("Length", mutetime + " minute(s)")
-  .addField("Reason", reason);
-
-  let channel = message.guild.channels.find(c => c.name === "logs");
-  if(!channel) return message.reply("Please create a logs channel first!");
-  channel.send(muteembed);
-
-  await(tomute.addRole(muterole.id));
-
-setTimeout(function() {
-         tomute.removeRole(muterole.id); 
-        }, timeInt);
-
-//end of module
-}
-
-module.exports.help = {
-  name: "tempmute"
-}
+ 
   
 
       if(command === "mute") {
