d0358dd3aaea0c04a544659c59ca71190620bca4
BuildTools <unconfigured@null.spigotmc.org>
2020-04-04 13:51:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -172,7 +172,8 @@ client.on("message", async message => {
 }
 
     if (command === "say") {
- if (message.member.hasPermission('MANAGE_MESSAGES')){
+ if (!message.member.hasPermission('MANAGE_MESSAGES'))
+            return message.reply("I won't let you take control of my body!");
             // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
             // To get the "message" itself we join the `args` back into a string with spaces: 
             const sayMessage = args.join(" ");
@@ -181,7 +182,7 @@ client.on("message", async message => {
             // And we get the bot to say the thing: 
             message.channel.send(sayMessage);
  }
-	} else return message.reply("I won't let you control my body!")
+	} else return message.reply()
 
     if (command === "kick") {
         // This command must be limited to the roles we wanna choose. In this example we just hardcode the role names.
