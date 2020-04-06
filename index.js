479f915c7665d962b4df6253aa9556c88f900dec
BuildTools <unconfigured@null.spigotmc.org>
2020-04-06 13:43:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ client.on("message", async message => {
 	poop.addRole(firstwarning.id)
      } else if(message.mentions.members.first().roles.some(r => ["1 Warning"].includes(r.name))) {
 	 poop.addRole(secondwarning.id)
-	 } else (message.mentions.members.first().roles.some(r => ["2 Warnings"].includes(r.name)))  {
+	 } else if(message.mentions.members.first().roles.some(r => ["2 Warnings"].includes(r.name)))  {
 	 poop.addRole(thirdwarning.id)
 	 } 
 	 }
