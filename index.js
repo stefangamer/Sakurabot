9513ddf9671f2244cf60d57178b60a930d7f3b6f
BuildTools <unconfigured@null.spigotmc.org>
2020-04-06 14:31:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -210,10 +210,13 @@ message.delete()
             // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
             // To get the "message" itself we join the `args` back into a string with spaces: 
             let channel = message.mentions.channels.first()
+			let sayMessage = "Nill"
 			if(!channel) {
 			channel = message.channel
+			sayMessage = args.join(" ");
+			} else {
+			sayMessage = args.slice(1).join(' ');
 			}
-			let sayMessage = args.slice(1).join(' ');
             // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley 
             // And we get the bot to say the thing: 
             channel.send(sayMessage);
