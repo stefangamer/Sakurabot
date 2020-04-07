64eefb1402eaa0711df1cd72ff9603fb166fd8e1
BuildTools <unconfigured@null.spigotmc.org>
2020-04-07 15:10:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -426,7 +426,7 @@ client.on("message", async message => {
     if(!!nickedperson && !message.member.hasPermission('MANAGE_NICKNAMES')) return message.channel.send("You don't have permissions to change others nicknames")
     if(!nickedperson) {
 	nickedperson = message.member
-	nick = args.slice(1).join(' ');
+	nick = args.join(' ');
 	}
     if(!nick) return message.channel.send("Please provide a nickname")
 	if(!!nickedperson) {
